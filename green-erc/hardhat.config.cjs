180425ff7c244529663c15dae4ce4a809f43f038
Levko123 <horvathlevente04@gmail.com>
2026-04-16 18:18:24 +0200

    chore: update dependencies
    - green-dapp: add @account-kit/react, wagmi, viem packages
    - green-erc: hardhat config update

--- green-erc/hardhat.config.cjs
+++ green-erc/hardhat.config.cjs
@@ -9,6 +9,11 @@ const networks = {
     url: process.env.LOCALHOST_RPC_URL || "http://127.0.0.1:8545",
     chainId: 31337,
   },
+  sepolia: {
+    url: process.env.SEPOLIA_RPC_URL || "",
+    accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    chainId: 11155111,
+  },
 };
 
 module.exports = {
